b08b6f9e7c47fec8d5f0c68bda4b1b5aa1b32d1e
vignesh <64148081+poppyseed64@users.noreply.github.com>
2020-08-25 18:50:34 +0530

    first prototype

---
+++ pet shop company website/app.js
@@ -0,0 +1,25 @@
+function contact(){
+    window.scroll(0,3500);
+
+}
+function pricing(){
+    window.scroll(0,2200);
+
+}
+function download(){
+    window.scroll(0,3500);
+
+}
+function redirect()
+{
+var url = "./signup.html";
+window.location.href=url;
+}
+function homeredirect(){
+    var url = "./index.html";
+ window.location.href=url;
+}
+function loginredirect(){
+    var url = "./login.html";
+ window.location.href=url;
+}
